d4b49bbf8845fb90845416d27043c07dce984a1d
mohamedfazilk <2114921@cognizant.com>
2022-08-20 11:20:11 +0530

    importing the react-router

--- src/App.js
+++ src/App.js
@@ -1,9 +1,14 @@
 import React from "react";
-
+import {BrowserRouter as Router,Route,Switch} from
+'react-router-dom'
+// importing the Pages
 import About from "./pages/About";
 import Home from "./pages/Home";
 import SingleCocktail from "./pages/SingleCocktail";
 import Error from "./pages/Error";
+
+//importing the Components
+import Navbar from './components/Navbar'
 
 function App() {
   return (
