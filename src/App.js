98c8607ccee78c179528cbf4273a15b44dc45955
mohamedfazilk <2114921@cognizant.com>
2022-08-20 11:49:33 +0530

    refactor the switch,Router because of older versions

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React from "react";
-import {BrowserRouter as Router,Route,Switch} from
+import { BrowserRouter, Routes, Route } from
 'react-router-dom'
 // importing the Pages
 import About from "./pages/About";
@@ -12,7 +12,18 @@ import Navbar from './components/Navbar'
 
 function App() {
   return (
-    <h2>gvgggg</h2>
+    <BrowserRouter>
+      <Navbar/>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route  path="/about" element={<About/>}/>
+        <Route  path="/cocktail/:id" element={<SingleCocktail/>}/>
+        <Route  path="*" element={<Error/>}/>
+         
+     
+        
+      </Routes>
+    </BrowserRouter>
   );
 }
 
