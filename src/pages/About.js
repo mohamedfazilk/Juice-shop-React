2ecc916a32ee896d5c9b1e483450b48ced286c3c
mohamedfazilk <2114921@cognizant.com>
2022-08-20 13:26:14 +0530

    import CocktailList and SearchForm into home

--- src/pages/About.js
+++ src/pages/About.js
@@ -2,7 +2,18 @@ import React from 'react'
 
 const About = () => {
   return (
-    <div>About</div>
+    <section className="section about-section">
+      <h1 className="section-title">About Us</h1>
+      <div className="about-text">
+      <p>
+        Lorem ipsum dolor sit amet, consectetur adipisicing elit. 
+        Soluta et vitae praesentium ipsum! In fuga, animi, 
+        molestiae aut repudiandae ea omnis harum dolorum quaerat 
+        earum mollitia nam ex facere doloribus.
+      </p>
+      </div>
+     
+    </section>
   )
 }
 
