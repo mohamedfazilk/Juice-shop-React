47d72de1cc84f51389187fae65741084aa1b671d
mohamedfazilk <2114921@cognizant.com>
2022-08-20 11:03:52 +0530

    create context Api and wrap into the index.js place the useGlobalContext

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+
+import About from "./pages/About";
+import Home from "./pages/Home";
+import SingleCocktail from "./pages/SingleCocktail";
+import Error from "./pages/Error";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <h2>gvgggg</h2>
   );
 }
 
