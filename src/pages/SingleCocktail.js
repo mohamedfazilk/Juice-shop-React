ce38ac22c2bac1d4424ab8950a22bf7122a82e4c
mohamedfazilk <2114921@cognizant.com>
2022-08-21 15:23:32 +0530

    place the objects in return

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -14,7 +14,7 @@ const SingleCocktail = () => {
       try {
         const response = await fetch(`${url}${id}`)
         const data = await response.json();
-        console.log(data);
+        
 
         if (data.drinks) {
           const { strDrink: name,
@@ -68,8 +68,48 @@ const SingleCocktail = () => {
     return <h2 className='section-title'>No Juices to Display</h2>
   }
 
+  const { name,
+    image,
+    info,
+    category,
+    glass,
+    instructions,
+    ingredients} = cocktail;
+
   return (
-    <div>{id}</div>
+    <section  className='section cocktail-section'>
+      <Link to='/' className='btn btn-primary'>Back to Home</Link>
+      <h2 className='section-title'>{name}</h2>
+      <div className="drink">
+        <img src={image} alt={name} />
+        <div className="drink-info">
+        <p>
+              <span className='drink-data'>name :</span> {name}
+            </p>
+            <p>
+              <span className='drink-data'>category :</span> {category}
+            </p>
+            <p>
+              <span className='drink-data'>info :</span> {info}
+            </p>
+            <p>
+              <span className='drink-data'>glass :</span> {glass}
+            </p>
+            <p>
+              <span className='drink-data'>instructons :</span> {instructions}
+            </p>
+
+            <p>
+              <span className='drink-data'>ingredients :</span> 
+              {ingredients.map((item,index)=>{
+                return item ? <span key={index}>{item}</span>:null
+              })}
+            </p>
+           
+        </div>
+      </div>
+     
+    </section>
   )
 }
 
