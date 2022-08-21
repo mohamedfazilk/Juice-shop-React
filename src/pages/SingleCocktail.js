092c7722273a5c9dbf27ef52110b4e81963d8b94
mohamedfazilk <2114921@cognizant.com>
2022-08-21 14:59:52 +0530

    place all 5 ingredients into one variable name

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -5,33 +5,43 @@ import { useParams, Link } from 'react-router-dom';
 const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
-  const {id} = useParams();
-  const [loading,setLoading] = React.useState(false);
-  const [cocktail,setCocktail] = React.useState(null);
+  const { id } = useParams();
+  const [loading, setLoading] = React.useState(false);
+  const [cocktail, setCocktail] = React.useState(null);
 
-  React.useEffect(()=>{
-    async function getCocktail(){
-      try{
+  React.useEffect(() => {
+    async function getCocktail() {
+      try {
         const response = await fetch(`${url}${id}`)
         const data = await response.json();
         console.log(data)
       }
-      catch(error){
+      catch (error) {
         console.log(error);
       }
 
-      if(data.drinks){
-        const{strDrink:name, strDrinkThumb:image, strAlcoholic:info, 
-          strCategory:category, strGlass:glass,
-        StrInstructions:instructions,
-        strIngredient1,
-        strIngredient2,
-        strIngredient3,
-        strIngredient4,
-        strIngredient5,
-      } = data.drinks[0];
+      if (data.drinks) {
+        const { strDrink: name,
+          strDrinkThumb: image
+          ,strAlcoholic: info,
+          strCategory: category,
+          strGlass: glass,
+          StrInstructions: instructions,
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5
+        } = data.drinks[0];
+        const ingredients = [
+          strIngredient1,
+          strIngredient2,
+          strIngredient3,
+          strIngredient4,
+          strIngredient5,
+        ]
       }
-      else{
+      else {
         setCocktail(null)
       }
     }
