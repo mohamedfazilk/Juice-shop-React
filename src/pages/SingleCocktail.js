1d845c60ca7bcb3bf615923cc080666decb12c44
mohamedfazilk <2114921@cognizant.com>
2022-08-21 15:05:58 +0530

    change the mistake by putting the if condition into the try catch

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -14,44 +14,47 @@ const SingleCocktail = () => {
       try {
         const response = await fetch(`${url}${id}`)
         const data = await response.json();
-        console.log(data)
+        console.log(data);
+
+        if (data.drinks) {
+          const { strDrink: name,
+            strDrinkThumb: image
+            ,strAlcoholic: info,
+            strCategory: category,
+            strGlass: glass,
+            StrInstructions: instructions,
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5
+          } = data.drinks[0];
+          const ingredients = [
+            strIngredient1,
+            strIngredient2,
+            strIngredient3,
+            strIngredient4,
+            strIngredient5,
+          ]
+          const newCocktail = {
+            name,
+            image,
+            info,
+            category,
+            glass,
+            instructions,
+            ingredients
+          }
+  
+          setCocktail(newCocktail);
+        }
+        else {
+          setCocktail(null)
+        }
       }
       catch (error) {
         console.log(error);
-      }
-
-      if (data.drinks) {
-        const { strDrink: name,
-          strDrinkThumb: image
-          ,strAlcoholic: info,
-          strCategory: category,
-          strGlass: glass,
-          StrInstructions: instructions,
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5
-        } = data.drinks[0];
-        const ingredients = [
-          strIngredient1,
-          strIngredient2,
-          strIngredient3,
-          strIngredient4,
-          strIngredient5,
-        ]
-        const newCocktail = {
-          name,
-          image,
-          info,
-          category,
-          glass,
-          instructions,
-          ingredients
-        }
-      }
-      else {
-        setCocktail(null)
+        setLoading(false)
       }
     }
     getCocktail();
