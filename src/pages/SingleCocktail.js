38d009aaa1399060901c82e031132c13601b36c0
mohamedfazilk <2114921@cognizant.com>
2022-08-21 12:53:19 +0530

    place the two state values for loading and cocktail

--- src/pages/SingleCocktail.js
+++ src/pages/SingleCocktail.js
@@ -1,8 +1,16 @@
-import React from 'react'
+import React from 'react';
+import Loading from '../components/Loading'
+import { useParams, Link } from 'react-router-dom';
+
+const url = 'https://www.thecocktaildb.com/api/json/v1/1/lookup.php?i='
 
 const SingleCocktail = () => {
+  const {id} = useParams();
+  const [loading,setLoading] = React.useState(false);
+  const [cocktail,setCocktail] = React.useState(null);
+
   return (
-    <div>SingleCocktail</div>
+    <div>{id}</div>
   )
 }
 
